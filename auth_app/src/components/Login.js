4be1186d584aac0efab273591f1821c505cc4306
vy3191 <venkyafhs@gmail.com>
2019-01-26 21:31:42 -0500

    Implemented sigin method front-end side

--- auth_app/src/components/Login.js
+++ auth_app/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Login extends Component {
   
@@ -6,13 +7,36 @@ export default class Login extends Component {
      username: '',
      password: ''
   }
+  handleInput = (event) => {
+     const target = event.target;
+     this.setState({
+        [target.name] : target.value
+     })
+  }
+  handleSubmit = (event) => {
+     event.preventDefault();
+     const credentials = this.state;
+     const endpoint = 'http://localhost:3300/api/login';
+     axios.post(endpoint, credentials)
+          .then(response => {
+             localStorage.setItem('jwt', response.data.token);
+          })
+          .catch(err => {
+             console.log(`errorMessage: `, err);
+          });
+      this.setState({
+         username: '',
+         password:''
+      })    
+  }  
   render() {
     return (
       <div>
         <from>
         <div>         
             <label htmlFor='username'>Username:</label>
-            <input type='text' name='username'></input>
+            <input type='text' name='username'
+                   value={this.state.username}></input>
          </div>
          <div>
             <label htmlFor='password'>Password:</label>
